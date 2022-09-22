8ccfae66ba4b080a6c6122792477fd3786416833
Александр Кузнецов <yakush4610@gmail.com>
2022-09-22 16:03:28 +1000

    Create Button1

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Kuznetsov
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Hello";
+        }
     }
 }
